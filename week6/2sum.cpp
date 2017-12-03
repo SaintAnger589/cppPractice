d2ce5c3afcf598dcd045c5a615d3d0e1855e123a
saint_anger <gauravsri589@gmail.com>
2017-12-02 22:49:34 -0800

    2sum corrected working

--- week6/2sum.cpp
+++ week6/2sum.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 #define nodes 1000000
-#define nodes 8
+//#define nodes 8
 
-void printarr(double arr[], int size){
+void printarr(long long arr[], int size){
 	int len = size;
 	int i;
 	for (i=0;i<len;i++){
@@ -15,7 +15,7 @@ void printarr(double arr[], int size){
 	}
 }
 
-void merge(double arr[], int l, int m, int r){
+void merge(long long arr[], int l, int m, int r){
 	int i = 0;
 	int j = 0;
 	int p = l;
@@ -23,8 +23,8 @@ void merge(double arr[], int l, int m, int r){
 	int temp;
 	int n1 = m - l + 1;
 	int n2 = r - m;
-	double *arr1 = (double *)malloc(sizeof(double)*n1);
-	double *arr2 = (double *)malloc(sizeof(double)*n2);
+	long long *arr1 = (long long *)malloc(sizeof(long long)*n1);
+	long long *arr2 = (long long *)malloc(sizeof(long long)*n2);
 
 	for (temp = 0;temp<n1;temp++){
 		arr1[temp] = arr[temp + l];
@@ -52,7 +52,7 @@ void merge(double arr[], int l, int m, int r){
 	//return res;
 }
 
-void partition(double arr[], int l, int r){
+void partition(long long arr[], int l, int r){
 	if (l < r){
 		int m = l + (r-l)/2;
 		//cout<<"m = "<<m<<endl;
@@ -64,19 +64,21 @@ void partition(double arr[], int l, int r){
 }
 
 
-int findtargetsum(double target, double initial, double arr[], int arr_size){
+bool findtargetsum(long long target, long long initial, long long arr[], int arr_size, int visited[]){
 	//cout<<"initial = "<<initial<<endl;
-	double num_to_find = target - initial;
+	long long num_to_find = target - initial;
 	if (num_to_find == initial)
-		return 0;
+		return false;
 	//binary search to the sorted array
 	int l = 0;
 	int flag = 0;
 	int r = arr_size;
 	while (l < r){
 		int m = (l + r)/2;
-		if (arr[m] == num_to_find){
+		if (arr[m] == num_to_find && visited[m] == 0){
 			flag = 1;
+			visited[m] = 1;
+			//visited[initial] = 1;
 			break;
 		}
 		else if (arr[m] > num_to_find)
@@ -87,19 +89,39 @@ int findtargetsum(double target, double initial, double arr[], int arr_size){
 
 	if (flag==1){
 		//cout<<"num_to_find = "<<num_to_find<<endl;
-	    return 1;
+		//cout<<"target = "<<target<<"\n"; 
+	    return true;
 	}
 	else 
-		return 0;
+		return false;
 }
+
+int removeDuplicates(long long A[], int n) {
+    if(n < 2) return n;
+    int id = 1;
+    for(int i = 1; i < n; ++i) 
+        if(A[i] != A[i-1]) A[id++] = A[i];
+    return id;
+}
+
+void resetvisited(int visited[], int arr_size){
+	for (int iter = 0;iter<arr_size;iter++){
+		visited[iter] = 0;
+	}
+}
+
 int main(){
 
-	double *arr = (double *)malloc(sizeof(double)*nodes);
-	double arrint;
+	long long *arr = (long long *)malloc(sizeof(long long)*nodes);
+	int *visited = (int *)malloc(sizeof(int)*nodes);
+	for (int iter = 0;iter<nodes;iter++){
+		visited[iter] = 0;
+	}
+	long long arrint;
 	ifstream myfile;
 	int k = 0;
-	//myfile.open("week6data.txt");
-	myfile.open("temp.txt");
+	myfile.open("week6data.txt");
+	//myfile.open("temp.txt");
 	if (myfile.is_open()){
 		while (myfile >> arrint){
 			//cout<<arrint<<endl;
@@ -114,18 +136,47 @@ int main(){
 
 	int *t;
 	int arr_size = k;
-	cout<<"arr_size = "<<arr_size<<endl;
-	printarr(arr,arr_size);
+	//cout<<"arr_size = "<<arr_size<<endl;
+	//printarr(arr,arr_size);
 	//sort the array by mergesort
 	partition(arr,0,arr_size-1);
 	//printitng the array
 	//printarr(arr, arr_size);
+
+	//remove duplicates
+	arr_size = removeDuplicates(arr,arr_size);
+	//printarr(arr,arr_size);
 	int i;
-	double target = 10;
+	long long target = -10000;
+	//long long target = 10;
 	int count = 0;
-	for (i=0;i<arr_size/2;i++){
-		count += findtargetsum(target, arr[i],arr, arr_size);
-	}
+	int flag_found = 0;
+	for (int j = 0;j<=20000;j++){
+	//for (int j = 0;j<=2;j++){
+		long long target1 = target + j;
+		flag_found = 0;
+		resetvisited(visited,arr_size);
+		for (i=0;i<arr_size;i++){
+		//i = arr_size-1;
+			if (flag_found == 1)
+				break;
+			// for (int j = i+1; j<arr_size;j++){
+			// 	if ((arr[i] + arr[j]) >=-10000 && (arr[i] + arr[j]) <= 10000){
+			// 	    //if (arr[i])
+			// 	    count++;
+			// 	    //cout<<count<<endl;
+			//     }
+			// }
+			//visited[i] = 1;
+			
+		    if (findtargetsum(target1, arr[i],arr, arr_size, visited)){
+		    // if (count > 
+		    //     cout<<"count = "<<count<<endl;
+		    	flag_found = 1;
+		    	count++;
+	        }
+	    } 
+    }
 	cout<<"count = "<<count<<endl;
 
 }
